5c413435084d68b55b669f0bb234e15aa1954bdc
Joshua Prince <goosemonkey97@gmail.com>
2012-03-04 21:44:40 -0800

    #1: First commit

---
+++ src/me/goosemonkey/Scores/Scores.java
@@ -0,0 +1,17 @@
+package me.goosemonkey.Scores;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Scores extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		this.registerListeners();
+	}
+	
+	private void registerListeners()
+	{
+		
+	}
+}
