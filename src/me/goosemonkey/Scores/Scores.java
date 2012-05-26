2d74b9ee406eaccd42afee07c5b80b6a373f5e10
SnRolls <snrolls@gmail.com>
2012-05-26 23:49:27 +0300

    Update src/me/goosemonkey/Scores/Scores.java

--- src/me/goosemonkey/Scores/Scores.java
+++ src/me/goosemonkey/Scores/Scores.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Scores extends JavaPlugin
 {
+	public final Logger log = Logger.getLogger("Minecraft");
+	
 	@Override
 	public void onEnable()
 	{
